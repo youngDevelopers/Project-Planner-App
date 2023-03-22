60aa9c367efc87667a8a145932fe43cb67cee1e4
youngDevelopers <dambaemmanuel5@gmail.com>
2023-03-22 11:20:04 +0200

    Setting up Create Project Form

--- src/components/projects/CreateProject.jsx
+++ src/components/projects/CreateProject.jsx
@@ -1,11 +1,47 @@
-import React, { Component } from 'react'
+import React, {useState} from 'react'
 
-export default class CreateProject extends Component {
-    render() {
-        return (
-            <div>
-                CreateProject
-            </div>
-        )
+function CreateProject() {
+
+    const [ title, setTitle ] = useState('');
+    const [ content , setContent ] = useState('');
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault()
+
+        console.log({
+            "title": title,
+            "content": content,
+        })
     }
+
+    const handleChange = (e) => {
+        if(e.target.id === "title" ){
+            setTitle(e.target.value)
+        };
+        if(e.target.id ==="content"){
+            setContent(e.target.value)
+        }
+    }
+
+    return (
+        <div className="container" >
+            <form onSubmit={handleFormSubmit} className="white">
+                <h5 className="grey-text text-darken-3 "> Sign In </h5>
+                <div className="input-field">
+                    <label htmlFor="title">Project Title</label>
+                    <input type="text" id="title" onChange={handleChange} />
+                </div>
+                <div className="input-field">
+                    <label htmlFor="content">Project Description</label>
+                    <input type="text" id="content" onChange={handleChange} />
+                </div>
+                <div className="input-field">
+                    <button className="btn pink lighten-1 z-depth-0">Create Project</button>
+                </div>
+            </form> -
+            
+        </div>
+    )
 }
+
+export default CreateProject
