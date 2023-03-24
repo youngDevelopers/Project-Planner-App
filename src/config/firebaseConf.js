05218fc67d70692c50809c75cd715f7d296fb882
youngDevelopers <dambaemmanuel5@gmail.com>
2023-03-24 11:48:51 +0200

    Updated Connect

--- src/config/firebaseConf.js
+++ src/config/firebaseConf.js
@@ -21,6 +21,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-fbStore= app.firestore().settings({timestampsInSnapshots:true});//initialise the firestore
+const fbStore= app.firestore().settings({timestampsInSnapshots:true});//initialise the firestore
 
 export default fbStore;
