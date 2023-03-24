0998d154bf3a521c4db1037c10efba4c5a32f527
youngDevelopers <dambaemmanuel5@gmail.com>
2023-03-24 11:10:28 +0200

    Thunk

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -3,6 +3,7 @@ export const createProject = ( project ) => {
     return (dispatch, getState, {getFirebase, getFirestore}) => { //fiirstly we pause a dispatch and later call a dispatch 
         //make an asyc call from the database
         const firestoreDB = getFirestore();
+        console.log("Thunk halting the dispatched action")
         /*firestoreDB.collection('projects').add({
             ...project,
             authorFirstName: 'Emmanuel',
