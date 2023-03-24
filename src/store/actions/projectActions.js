e32d1d306e2a910744d799d8ac11f5ab7532732b
youngDevelopers <dambaemmanuel5@gmail.com>
2023-03-24 21:04:50 +0200

    Updated action

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -3,20 +3,22 @@ import { collection, addDoc } from "firebase/firestore";
 const createProject = async ( project ) => {
     return (dispatch, getState, {getFirebase, getFirestore}) => { //fiirstly we pause a dispatch and later call a dispatch 
         //make an asyc call from the database
-        const db = getFirestore()
-        try {
-            const docRef = await addDoc(collection(db, "projects"), {
+        const getPromise = () =>{
+            addDoc(collection(db, "projects"), {
                 ...project,
                 authorFirstName: 'Emmanuel',
                 authorLastName: 'Damba',
                 authorId: 12345,
                 createdAt: new Date(),
-            });
-            dispatch({
+            })
+        }
+        const db = getFirestore()
+        try {
+            const docRef =  getPromise();
+            return dispatch({
                 type: 'CREATE PROJECT',
                 project,
             });
-            console.log("Document written with ID: ", docRef.id);
           } catch (e) {
             console.error("Error adding document: ", e);
           }
