181db921bc8e3651d0e951e2be3c300cf0c0b950
youngDevelopers <dambaemmanuel5@gmail.com>
2023-03-24 14:32:00 +0200

    Updated the Async Call

--- src/config/firebaseConf.js
+++ src/config/firebaseConf.js
@@ -1,8 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import 'firebase/firestore';
-import 'firebase/auth';
-import { getAnalytics } from "firebase/analytics";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,7 +18,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const fbStore= app.firestore().settings({timestampsInSnapshots:true});//initialise the firestore
 
-export default fbStore;
+export default app;
+
--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,29 +1,26 @@
+import { collection, addDoc } from "firebase/firestore";
 
-const createProject = ( project ) => {
+const createProject = async ( project ) => {
     return (dispatch, getState, {getFirebase, getFirestore}) => { //fiirstly we pause a dispatch and later call a dispatch 
         //make an asyc call from the database
-        const firestoreDB = getFirestore();
-        firestoreDB.collection('projects').add({
-            ...project,
-            authorFirstName: 'Emmanuel',
-            authorLastName: 'Damba',
-            authorId: 12345,
-            createdAt: new Date(),
-        }).then( () => {
+        const db = getFirestore()
+        try {
+            const docRef = await addDoc(collection(db, "projects"), {
+                ...project,
+                authorFirstName: 'Emmanuel',
+                authorLastName: 'Damba',
+                authorId: 12345,
+                createdAt: new Date(),
+            });
             dispatch({
                 type: 'CREATE PROJECT',
                 project,
             });
-        } ).catch(
-            (err) =>{
-                dispatch(
-                    {
-                        type: 'Create Project Error',
-                        err,
-                    }
-                )
-            }
-        )
+            console.log("Document written with ID: ", docRef.id);
+          } catch (e) {
+            console.error("Error adding document: ", e);
+          }
+        
     }
 };
 
